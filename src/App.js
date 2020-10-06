42e5cc5df9a3bf13cac551d39a79cc3601507b2f
jahanavi9 <tanishsai299>
2020-10-06 10:57:19 -0500

    app.js file chnages

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
 import Footer from './component/Footer';
-
 import Header from './component/Header';
 import Quitz from './component/Quitz';
-
 
 function App() {
   return (
     <div className="App">
-     
-     
       <Header projectname="TekBits"></Header>
-    
-    <Quitz></Quitz>
-    
-     
+      <Quitz></Quitz>
       <Footer></Footer>
     </div>
   );
 }
 
 export default App;
-
-
