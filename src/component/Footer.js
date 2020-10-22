1894f47e462b1531f5b979c9475e6bae26774928
jahanavi9 <github2test@gmail.com>
2020-10-21 22:18:28 -0500

    added ux

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,8 +3,17 @@ import React from 'react';
 function Footer(props) {
     return (
         <div>
-         <h4><b>all rights reserved</b></h4> 
+        <div className="circle"><div className="t"><h1>T</h1></div></div>
+        <div className="rec1"></div>
+        <div className="rec2"></div>
+        <div className="rec3"></div>
+        
+
+        <div className="bottom">
+
+        <span><h4>Developers behind the code<br></br>priya,sowmya,jahnavi,chaitanya</h4></span></div>
         </div>
+        
     );
 }
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -5,6 +5,7 @@ function Header(props) {
         <div>
             <h3>{props.projectname}</h3>
             Welcome to {props.projectname}
+            <hr color =" rgb(211, 126, 70)" width="80%" ></hr>
           
         </div>
     );
